b50b625f16725ee9a1f1c471965591a29d3b9684
takalivian <61145524+takalivian@users.noreply.github.com>
2020-10-20 18:15:35 +0900

    user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,50 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
+  has_many :sns_credentials
+  has_many :posts
+  has_many :comments
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverse_of_relationships, source: :user
+  has_many :messages, dependent: :destroy
+  has_many :entries, dependent: :destroy
+
+  mount_uploader :image, ImageUploader
+
+  validates :name, presence: true, uniqueness: true, length:{maximum: 10}
+  
+  # def self.from_omniauth(auth)
+  #   sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+  #   user = sns.user || User.where(email: auth.info.email).first_or_initialize(
+  #     name: auth.info.name,
+  #       email: auth.info.email
+  #   )
+  #   if user.persisted?
+  #     sns.user = user
+  #     sns.save
+  #   end
+  #   { user: user, sns: sns }
+  # end
+
+  # フォロー
+  def follow(other_user)
+    unless self == other_user
+      self.relationships.find_or_create_by(follow_id: other_user.id)
+    end
+  end
+
+  def unfollow(other_user)
+    relationship = self.relationships.find_by(follow_id: other_user.id)
+    relationship.destroy if relationship
+  end
+
+  def following?(other_user)
+    self.followings.include?(other_user)
+  end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :agehash
 end
