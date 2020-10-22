c2eba3059bf1698f8bca00451bf8fd2465d62809
takalivian <61145524+takalivian@users.noreply.github.com>
2020-10-22 13:56:19 +0900

    Japanese

--- config/application.rb
+++ config/application.rb
@@ -6,14 +6,21 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Cosme
+module Mencos
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
   end
 end
